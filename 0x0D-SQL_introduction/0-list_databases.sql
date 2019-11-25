87e5919be2fdb01b0f2bca6776d3d2ee6a693bc8
chriswill88 <williechri79@gmail.com>
2019-11-25 14:00:42 -0500

    [test]

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,2 @@
--- this is the first sql script i am creating.
--- this script should list all databases
-SHOW DATABASES;
+-- this is the first sql script i am creating. this script should list all databases
+SHOW DATABASES
