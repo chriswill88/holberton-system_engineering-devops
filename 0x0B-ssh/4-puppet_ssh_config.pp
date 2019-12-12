6287321c25c9bc1e090b7c2476392928c58f81b3
chriswill88 <williechri79@gmail.com>
2019-12-12 13:46:17 -0500

    fix 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
-# this is a puppet file
+# This is a puppet file
 
 file_line { 'id file stuff':
-  path => /etc/ssh/ssh_config,
+  path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/holberton',
 }
 
 file_line { 'psw no':
-  path => /etc/ssh/ssh_config,
+  path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
