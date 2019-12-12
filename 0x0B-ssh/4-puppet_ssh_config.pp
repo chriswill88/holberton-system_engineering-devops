0da02f603dfbb69d33f8552de0278dfa006030a9
chriswill88 <williechri79@gmail.com>
2019-12-12 11:30:41 -0500

    [ADD] task 4

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# this is a puppet file
+
+file_line {
+  ensure => present,
+  path  => /etc/ssh/ssh_config,
+  line  => '\tIdentityFile ~/.ssh/holberton',
+}
+
+file_line {
+  ensure => present,
+  path => /etc/ssh/ssh_config,
+  line => '\tPasswordAuthentication no',
+  match => 'PasswordAuthentication',
+}
