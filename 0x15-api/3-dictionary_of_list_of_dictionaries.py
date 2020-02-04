953113c922fc424b2846af483b6227fff0dd679f
chriswill88 <williechri79@gmail.com>
2020-02-03 23:50:03 -0500

    [ADD] task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import json
+import requests
+"""parses info from api"""
+
+if __name__ == "__main__":
+    USER_ID = ""
+    USERNAME = ""
+    TASK_COMPLETED_STATUS = ""
+    TASK_TITLE = ''
+    r = requests.get('https://jsonplaceholder.typicode.com/users')
+    a = requests.get('https://jsonplaceholder.typicode.com/todos')
+    listy = r.json()
+    newdict = {}
+
+    for i in listy:
+        USER_ID = i['id']
+        USERNAME = i["username"]
+        listy = a.json()
+        licty = []
+        num = 0
+        for i in listy:
+            TASK_TITLE = i["title"]
+            TASK_COMPLETED_STATUS = str(i["completed"])
+            licty.append({})
+            licty[num]["username"] = USERNAME
+            licty[num]["task"] = TASK_TITLE
+            licty[num]["completed"] = TASK_COMPLETED_STATUS
+            num += 1
+        newdict[str(USER_ID)] = licty
+
+    with open('todo_all_employees.json'.format(USER_ID), 'w') as e:
+        json.dump(newdict, e)
