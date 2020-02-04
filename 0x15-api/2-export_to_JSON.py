061cb8f68f41ed8ce9cfcfb88bed740fff279d2c
chriswill88 <williechri79@gmail.com>
2020-02-03 23:19:33 -0500

    [ADD] task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import json
+import requests
+import sys
+"""parses info from api"""
+
+if __name__ == "__main__":
+    USER_ID = int(sys.argv[1])
+    USERNAME = ""
+    TASK_COMPLETED_STATUS = ""
+    TASK_TITLE = ''
+    # this pulls up the posts
+    r = requests.get('https://jsonplaceholder.typicode.com/users')
+    listy = r.json()
+    for i in listy:
+        if i['id'] == USER_ID:
+            rightdict = i
+            break
+    USERNAME = rightdict["username"]
+    title_list = []
+    a = requests.get('https://jsonplaceholder.typicode.com/todos')
+    listy = a.json()
+    # with open('{}.csv'.format(USER_ID), 'w', newline='') as e:
+    # write = csv.writer(e)
+    licty = []
+    newdict = {str(USER_ID): licty}
+    num = 0
+    for i in listy:
+        if i['userId'] == USER_ID:
+            TASK_TITLE = i["title"]
+            TASK_COMPLETED_STATUS = str(i["completed"])
+            licty.append({})
+            licty[num]["task"] = TASK_TITLE
+            licty[num]["completed"] = TASK_COMPLETED_STATUS
+            licty[num]["username"] = USERNAME
+            num += 1
+
+    with open('{}.json'.format(USER_ID), 'w') as e:
+        json.dump(newdict, e)
