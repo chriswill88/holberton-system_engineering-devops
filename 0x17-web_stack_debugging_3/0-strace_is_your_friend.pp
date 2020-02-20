0671138551f27889de18bda40187aa86c117ce45
chriswill88 <williechri79@gmail.com>
2020-02-20 02:04:59 -0500

    [UPDATE]

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,7 @@
 # this manifest fixes a server
 
-file_line { '/var/www/html/wp-settings.php':
-  ensure  => present,
-  path    => '/var/www/html/wp-settings.php',
-  replace => true,
-  line    => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-  match   => 'phpp',
+exec { 'sed':
+  command => "sed -i 's-phpp-php-g' /var/www/html/wp-settings.php",
+  path => "/bin",
 }
+
