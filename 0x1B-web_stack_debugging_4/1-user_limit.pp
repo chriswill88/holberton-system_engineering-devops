0504292e21e402611cf4a090cdf5a61b642ee32b
chriswill88 <williechri79@gmail.com>
2020-02-27 01:58:05 -0500

    [ADD] adv task 2

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # this manifest is for fixing an os error
 exec { 'limit':
-  command => 'sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 6000/g; s/holberton hard nofile 5/holberton hard nofile 6000/g" /etc/security/limits.conf',
+  command => 'sudo sed -i "s/nofile 4/nofile 6000/g; s/nofile 5/nofile 6000/g" /etc/security/limits.conf',
   path    => ['/bin', '/usr/sbin', '/usr/bin', '/usr'],
 }
